86fbf6ce6b271b8909a192b6558fdfac862068ce
Muhammad Rehan <mrehanmeo@gmail.com>
2022-09-05 02:54:40 +0500

    Initial Commit

---
+++ SimpleCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleCoreData
+//
+//  Created by Muhammad Rehan on 9/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
